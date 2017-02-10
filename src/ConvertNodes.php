0087f58f76f80d8f3de401a17c5f94b298f07b10
Eliot  Scott <eliotwsc@malt.umdl.umich.edu>
2017-02-10 16:02:10 -0500

    initial commit

---
+++ src/ConvertNodes.php
@@ -0,0 +1,236 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\convert_nodes\ConvertNodes.
+ */
+
+namespace Drupal\convert_nodes;
+
+use Drupal\Core\Field\WidgetBase;
+use Drupal\Core\Render\Markup;
+use Drupal\Core\Database\Database;
+
+class ConvertNodes {
+
+  public static function getFromFields($fields_from, $fields_to_names, $fields_to_types) {
+    $fields_from_names = [];
+    $form = [];
+    foreach ($fields_from as $field) {
+      $options = $fields_to_names;
+      foreach ($options as $option => $label) {
+        $val_name = $field->getFieldStorageDefinition()->getMainPropertyName(); //because might be target_id
+        if (!in_array($option, ['append_to_body', 'remove']) &&
+            $fields_to_types[$option] != $field->getFieldStorageDefinition()->getPropertyDefinition($val_name)->getDataType()) {
+          unset($options[$option]);
+        }
+      }
+      if($field->getFieldStorageDefinition()->isBaseField() == FALSE) {
+        $fields_from_names[] = $field->getName();
+        $form[$field->getName()] = [
+          '#type' => 'select',
+          '#title' => t('From Field ['.$field->getName().'] '.(is_object($field->getLabel()) ? $field->getLabel()->render():$field->getLabel()).':<br/> To Field'),
+          '#options' => $options,
+          '#default_value' => (array_key_exists($field->getName(),$fields_to_names) ? $field->getName():NULL),
+        ];
+      }
+    }
+    return array('fields_from_names' => $fields_from_names, 'fields_from_form' => $form);
+  }
+
+  public static function getToFields($fields_to) {
+    $fields_to_names = [];
+    $fields_to_types = [];
+    // add some extra options for the form
+    $fields_to_names['append_to_body'] = 'append_to_body';
+    $fields_to_names['remove'] = 'remove';
+    // get the to fields in an array
+    foreach ($fields_to as $field) {
+      if($field->getFieldStorageDefinition()->isBaseField() == FALSE) {
+        $val_name = $field->getFieldStorageDefinition()->getMainPropertyName(); //because might be target_id
+        $fields_to_names[$field->getName()] = '['.$field->getName().'] '.(is_object($field->getLabel()) ? $field->getLabel()->render():$field->getLabel());
+        $fields_to_types[$field->getName()] = $field->getFieldStorageDefinition()->getPropertyDefinition($val_name)->getDataType();
+      }
+    }
+    return array('fields_to_names' => $fields_to_names, 'fields_to_types' => $fields_to_types);
+  }
+
+  public static function getContentTypes() {
+    $contentTypes = \Drupal::service('entity.manager')->getStorage('node_type')->loadMultiple();
+    $contentTypesList = [];
+    foreach ($contentTypes as $contentType) {
+      $contentTypesList[$contentType->id()] = $contentType->label();
+    }
+    return $contentTypesList;
+  }
+
+  public static function getBaseTableNames() {
+    $storage = \Drupal::service('entity_type.manager')->getStorage('node');
+    // Get the names of the base tables.
+    $base_table_names = [];
+    $base_table_names[] = $storage->getBaseTable();
+    $base_table_names[] = $storage->getDataTable();
+    return $base_table_names;
+  }
+
+  public static function sortUserInput($user_input, $fields_new_to) {
+    // get user input and set up vars
+    $map_fields = array();
+    $update_fields = array();
+    // remove stuff we dont need
+    $unset_data = ['op','form_build_id','form_token','form_id'];
+    foreach ($user_input as $from => $to) {
+      if (in_array($from, $unset_data)) {
+        continue;
+      }
+      if ($from == $to) {
+        $update_fields[] = $from;
+      }
+      else if (in_array($from, $fields_new_to) && !in_array($from, $user_input)) {
+        $map_fields['create_new'] = array(
+          'field' => $from,
+          'value' => $to,
+        );
+      }
+      else {
+        $map_fields[$from] = array(
+          'field' => $to,
+          'value' => array(), //this will come in later
+        );
+      }
+    }
+    return array('map_fields' => $map_fields, 'update_fields' => $update_fields);
+  }
+
+  public static function getFieldTableNames($fields_from) {
+    $table_mapping = \Drupal::service('entity_type.manager')->getStorage('node')->getTableMapping();
+    $field_table_names = [];
+    foreach ($fields_from as $key => $field) {
+      if($field->getFieldStorageDefinition()->isBaseField() == FALSE) {
+        $field_name = $field->getName();
+        $field_table = $table_mapping->getFieldTableName($field_name);
+        $field_table_names[$field_name] = $field_table;
+        $field_storage_definition = $field->getFieldStorageDefinition();
+        $field_revision_table = $table_mapping->getDedicatedRevisionTableName($field_storage_definition);
+        // Field revision tables DO have the bundle!
+        $field_table_names[$field_name.'_revision'] = $field_revision_table;
+      }
+    }
+    return $field_table_names;
+  }
+
+  public static function getNids($from_type) {
+    // Get the node IDs to update.
+    $query = \Drupal::service('entity.query')->get('node');
+    $query->condition('type', $from_type);
+    $nids = $query->execute();
+    return $nids;
+  }
+
+  public static function getOldFieldValues($nids, $map_fields, $fields_to) {
+    foreach ($nids as $vid => $nid) {
+      $node = \Drupal\node\Entity\Node::load($nid);
+      foreach ($map_fields as $map_from => $map_to) {
+        if ($map_to['field'] == 'remove' || $map_from == 'create_new') { continue; }
+        $value = '';
+        // TODO Need to get multiple values
+        if ($node->$map_from) {
+          //because might be target_id
+          $val_name = $node->$map_from->getFieldDefinition()->getFieldStorageDefinition()->getMainPropertyName();
+          $value = $node->$map_from->$val_name;
+          // because datetime/date may need converting
+          // TODO date with time did not insert into date only fields
+          // need to test if date without time will insert into date with time
+          // or better yet, find a better way to do this
+          $from_type = $node->$map_from->getFieldDefinition()->getFieldStorageDefinition()->getType();
+          $to_type = $fields_to[$map_to['field']];
+          if (!empty($to_type) && in_array('datetime',array($to_type,$from_type))) {
+            $date = new \DateTime($value);
+            $value = $date->format('Y-m-d');
+          }
+        }
+        $map_fields[$map_from]['value'][$nid] = $value;
+      }
+    }
+    return $map_fields;
+  }
+
+  public static function convertBaseTables($nids, $base_table_names, $to_type, &$context) {
+    $message = 'Converting Base Tabless...';
+    $results = array();
+    $db = Database::getConnection();
+    // Base tables have 'nid' and 'type' columns.
+    foreach ($base_table_names as $table_name) {
+      $results[] = $db->update($table_name)
+        ->fields(['type' => $to_type])
+        ->condition('nid', $nids, 'IN')
+        ->execute();
+    }
+    $context['message'] = $message;
+    $context['results'] = $results;
+  }
+
+  public static function convertFieldTables($nids, $field_table_names, $to_type, $update_fields, &$context) {
+    $message = 'Converting Field Tabless...';
+    $results = array();
+    $db = Database::getConnection();
+    // Field tables have 'entity_id' and 'bundle' columns.
+    foreach ($field_table_names as $field_name => $table_name) {
+      // only do this when from and to fields are the same
+      if (in_array(str_replace('_revision','',$field_name), $update_fields)) {
+        $results[] = $db->update($table_name)
+          ->fields(['bundle' => $to_type])
+          ->condition('entity_id', $nids, 'IN')
+          ->execute();
+      }
+    }
+    $context['message'] = $message;
+    $context['results'] = $results;
+  }
+
+  public static function addNewFields($nids, $map_fields, &$context) {
+    //flush cache so we recognize new bundle type before updates
+    drupal_flush_all_caches();
+    $message = 'Adding Fields...';
+    $results = array();
+    foreach ($nids as $vid => $nid) {
+      $node = \Drupal\node\Entity\Node::load($nid);
+      foreach ($map_fields as $map_from => $map_to) {
+        if ($map_to['field'] == 'remove') { continue; }
+        if ($map_to['field'] == 'append_to_body') {
+          $body = $node->get('body')->getValue()[0];
+          $markup = Markup::create($body['value'].'<div class="field__label">'.$map_from.'</div><p>'.$map_to['value'][$nid].'</p>');
+          $node->get('body')->setValue([['value' => $markup, 'summary' => $body['summary'], 'format' => $body['format']]]);
+        }
+        else {
+          // TODO account for multiple values
+          $val_name = $node->$map_to['field']->getFieldDefinition()->getFieldStorageDefinition()->getMainPropertyName();
+          if ($map_from == 'create_new') {
+            $node->get($map_to['field'])->setValue([[$val_name => $map_to['value']]]);
+          }
+          else if (!empty($map_to['value'][$nid])) {
+            $node->get($map_to['field'])->setValue([[$val_name => $map_to['value'][$nid]]]);
+          }
+        }
+        $results[] = $node->save();
+      }
+    }
+    $context['message'] = $message;
+    $context['results'] = $results;
+  }
+
+  function ConvertNodesFinishedCallback($success, $results, $operations) {
+    // The 'success' parameter means no fatal PHP errors were detected. All
+    // other error management should be handled using 'results'.
+    if ($success) {
+      $message = \Drupal::translation()->formatPlural(
+        count($results),
+        'One operations processed.', '@count operations processed.'
+      );
+    }
+    else {
+      $message = t('Finished with an error.');
+    }
+    drupal_set_message($message);
+  }
+}
---
+++ src/Form/ConvertNodesForm.php
@@ -0,0 +1,213 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\convert_nodes\Form\ConvertNodesForm.
+ */
+
+namespace Drupal\convert_nodes\Form;
+
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormState;
+use Drupal\Core\Form\FormInterface;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Field\WidgetBase;
+use Drupal\Core\Render\Markup;
+use Drupal\convert_nodes\ConvertNodes;
+
+class ConvertNodesForm extends FormBase implements FormInterface {
+
+  // set a var to make this easier to keep track of
+  protected $step = 1;
+  // set some content type vars
+  protected $from_type = NULL;
+  protected $to_type = NULL;
+  // set field vars
+  protected $fields_from = NULL;
+  protected $fields_to = NULL;
+  // create new based on to content type
+  protected $create_new = NULL;
+  protected $fields_new_to = NULL;
+  // keep track of user input
+  protected $user_input = array();
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'convert_nodes_admin';
+  }
+
+  public function _convertNodes() {
+    $base_table_names = ConvertNodes::getBaseTableNames();
+    $user_input = ConvertNodes::sortUserInput($this->user_input, $this->fields_new_to);
+    $map_fields = $user_input['map_fields'];
+    $update_fields = $user_input['update_fields'];
+    $field_table_names = ConvertNodes::getFieldTableNames($this->fields_from);
+    $nids = ConvertNodes::getNids($this->from_type);
+    $map_fields = ConvertNodes::getOldFieldValues($nids, $map_fields, $this->fields_to);
+    $batch = array(
+      'title' => t('Converting Base Tables...'),
+      'operations' => array(
+        array('\Drupal\convert_nodes\ConvertNodes::convertBaseTables', array($nids, $base_table_names, $this->to_type)),
+        array('\Drupal\convert_nodes\ConvertNodes::convertFieldTables', array($nids, $field_table_names, $this->to_type, $update_fields)),
+        array('\Drupal\convert_nodes\ConvertNodes::addNewFields', array($nids, $map_fields)),
+      ),
+      'finished' => '\Drupal\convert_nodes\ConvertNodes::ConvertNodesFinishedCallback',
+    );
+    batch_set($batch);
+    return 'All nodes of type '.$this->from_type.' were converted to '.$this->to_type;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    switch ($this->step) {
+      case 1:
+        $form_state->setRebuild();
+        $this->from_type = $form['convert_nodes_content_type_from']['#value'];
+        $this->to_type = $form['convert_nodes_content_type_to']['#value'];
+        break;
+      case 2:
+        $form_state->setRebuild();
+        $this->user_input = $form_state->getUserInput();
+        break;
+      case 3:
+        $this->create_new = $form['create_new'];
+        if (empty($this->create_new)) {
+          goto five;
+        }
+        $form_state->setRebuild();
+        break;
+      case 4:
+        $this->user_input = array_merge($this->user_input, $form_state->getUserInput());
+        $form_state->setRebuild();
+        break;
+      case 5:
+      //used also for goto.
+      five:
+          if (method_exists($this, '_convertNodes')) {
+            $return_verify = $this->_convertNodes();
+          }
+          drupal_set_message($return_verify);
+          \Drupal::service("router.builder")->rebuild();
+        break;
+    }
+    $this->step++;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, \Drupal\Core\Form\FormStateInterface $form_state) {
+    if (isset($this->form)) { $form = $this->form; }
+    switch ($this->step) {
+      case 1:
+        drupal_set_message('This module is experiemental. PLEASE do not use on production databases without prior testing and a complete database dump.', 'warning');
+        // get content types and put them in the form
+        $contentTypesList = ConvertNodes::getContentTypes();
+        $form['convert_nodes_content_type_from'] = [
+          '#type' => 'select',
+          '#title' => t('From Content Type'),
+          '#options' => $contentTypesList,
+        ];
+        $form['convert_nodes_content_type_to'] = [
+          '#type' => 'select',
+          '#title' => t('To Content Type'),
+          '#options' => $contentTypesList,
+        ];
+        $form['actions']['submit'] = [
+          '#type' => 'submit',
+          '#value' => $this->t('Next'),
+          '#button_type' => 'primary',
+        ];
+        break;
+      case 2:
+        // get the fields
+        $entityManager = \Drupal::service('entity_field.manager');
+        $this->fields_from = $entityManager->getFieldDefinitions('node', $this->from_type);
+        $this->fields_to = $entityManager->getFieldDefinitions('node', $this->to_type);
+
+        $fields_to = ConvertNodes::getToFields($this->fields_to);
+        $fields_to_names = $fields_to['fields_to_names'];
+        $fields_to_types = $fields_to['fields_to_types'];
+
+        $fields_from = ConvertNodes::getFromFields($this->fields_from, $fields_to_names, $fields_to_types);
+        $fields_from_names = $fields_from['fields_from_names'];
+        $fields_from_form = $fields_from['fields_from_form'];
+
+        // find missing fields. allowing values to be input later
+        $fields_to_names = array_diff($fields_to_names, array('append_to_body','remove'));
+        $this->fields_new_to = array_diff(array_keys($fields_to_names), $fields_from_names);
+
+        $form = array_merge($form, $fields_from_form);
+        $form['actions']['submit'] = [
+          '#type' => 'submit',
+          '#value' => $this->t('Next'),
+          '#button_type' => 'primary',
+        ];
+        break;
+      case 3:
+        $form['create_new'] = array(
+          '#type' => 'checkbox',
+          '#title' => $this->t('Create field values for fields in new content type'),
+        );
+        $form['actions']['submit'] = [
+          '#type' => 'submit',
+          '#value' => $this->t('Next'),
+          '#button_type' => 'primary',
+        ];
+        break;
+      case 4:
+        $entityManager = \Drupal::service('entity_field.manager');
+        // put the to fields in the form for new values
+        foreach ($this->fields_new_to as $field_name) {
+          if (!in_array($field_name, $this->user_input)) {
+// TODO Need to figure out a way to get form element based on field def here
+// for now just a textfield
+/*
+$field = $entityManager->getFieldDefinitions('node', $this->to_type)[$field_name];
+$field_type = $field->getFieldStorageDefinition()->getType();
+$field_options = $field->getFieldStorageDefinition()->getSettings();
+$element = array (
+'#title' => 'test',
+'#description' => 'some desc',
+'#required' => true,
+'#delta' => 0,
+);
+$test = WidgetBase('test', 'test', $field, array(), array());
+$test->formElement($field, 0, $element, $form, $form_state);
+*/
+            $form[$field_name] = [
+              '#type' => 'textfield',
+              '#title' => t('Set Field ['.$field_name.']'),
+            ];
+          }
+        }
+        $form['actions']['submit'] = [
+          '#type' => 'submit',
+          '#value' => $this->t('Next'),
+          '#button_type' => 'primary',
+        ];
+        break;
+      case 5:
+        drupal_set_message('This module is experiemental. PLEASE do not use on production databases without prior testing and a complete database dump.', 'warning');
+        $form['actions']['submit'] = [
+          '#type' => 'submit',
+          '#value' => $this->t('Convert'),
+          '#button_type' => 'primary',
+        ];
+        break;
+    }
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function validateForm(array &$form, FormStateInterface $form_state) {
+    //TODO
+  }
+
+}
