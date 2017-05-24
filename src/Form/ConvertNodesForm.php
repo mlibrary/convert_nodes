6dca43bde93e12749b3b0eabdd6605e9e1c45544
lomasr <lomasr@357323.no-reply.drupal.org>
2017-05-24 12:52:17 -0400

    Issue #2878178 by lomasr: Validation for node count 0 for a "FROM" content type

--- src/Form/ConvertNodesForm.php
+++ src/Form/ConvertNodesForm.php
@@ -207,7 +207,13 @@ $test->formElement($field, 0, $element, $form, $form_state);
    * {@inheritdoc}
    */
   public function validateForm(array &$form, FormStateInterface $form_state) {
-    //TODO
+    $this->from_type = $form['convert_nodes_content_type_from']['#value'];
+    $query = \Drupal::entityQuery('node')->condition('type', $this->from_type);
+    $count_type = $query->count()->execute();
+    if ($count_type == 0) {
+      $form_state->setErrorByName('convert_nodes_content_type_from', $this->t('No content found to convert.'));
+    }
+
   }
 
 }
